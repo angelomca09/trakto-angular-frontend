f2b097996ed850c9919de65e3945a67682a61dc2
Angelo Munhoz <angelomca09@gmail.com>
2023-04-20 14:24:52 -0300

    Adding Theme to Header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'ap-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  @Input() theme: string = "dark";
+  themeClass = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.themeClass = {
+      [this.theme]: true
+    }
+  }
 
 }
