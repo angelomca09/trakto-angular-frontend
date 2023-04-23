ee084517f5af605fa6f5d5d0cc0f4eb728e52d8b
Angelo Munhoz <angelomca09@gmail.com>
2023-04-23 18:47:37 -0300

    Header Changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/core/user/user';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
   selector: 'ap-header',
@@ -10,12 +13,30 @@ export class HeaderComponent implements OnInit {
   @Input() theme: string = "dark";
   themeClass = {};
 
-  constructor() { }
+  user: User | null = null;
+
+  date: string;
+
+  showRedirectButton: boolean;
+
+  constructor(
+    private userService: UserService,
+    private router: Router) {
+    const date = new Date();
+    this.date = `${date.toLocaleDateString("pt-BR")}`;
+    this.user = userService.getUser();
+    this.showRedirectButton = !(router.url === "/home")
+  }
 
   ngOnInit(): void {
     this.themeClass = {
       [this.theme]: true
     }
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate([""]);
   }
 
 }
